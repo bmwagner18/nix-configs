5110f3853b25699ecec3f91c6ac18a64f0ef332c
Ben Wagner <64104085+bmwagner18@users.noreply.github.com>
2024-06-30 18:05:48 -0400

    add config for ben-pc

---
+++ hosts/ben-pc/home.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, userSettings, ... }: {
+
+  # Where Home Manager will home manage
+  home.username = userSettings.username;
+  home.homeDirectory = "/home/"+userSettings.username;
+
+  # Let it manage itself
+  programs.home-manager.enable = true;
+
+  imports = [
+    ../../user/shell/sh.nix # bash config
+    ../../user/app/git/git.nix # git config
+    ../../user/app/neovim/neovim.nix
+  ];
+
+  home.stateVersion = "23.11";
+
+  # Home Manager needs to know unfree is ok too
+  nixpkgs.config.allowUnfree = true;
+  # nixpkgs.allowUnfreePredicate = _: true;
+
+  home.packages = with pkgs; [
+    # Core
+    firefox
+    joplin-desktop
+    vscode
+    betterbird 
+    protonmail-bridge-gui
+
+    # Terminal
+    alacritty
+    zellij
+    lazygit
+
+    # Office
+    libreoffice-fresh
+    wine
+
+    # Media
+    jellyfin-media-player
+    vlc
+    obs-studio
+    ffmpeg
+    losslesscut-bin
+    spotify
+    yt-dlp-light
+    tartube
+
+    # Communication
+    slack
+    discord
+
+    # Engineering tools
+    super-slicer-latest
+
+    # Development
+    android-tools
+    android-udev-rules
+  ];
+
+}
